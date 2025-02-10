8f63e809423137f91534b5543c3910d4fc635b9f
davidhhhhhhh <57957067+davidhhhhhhh@users.noreply.github.com>
2025-02-10 17:51:41 -0500

    model refinements on nested residual models

--- r_scripts/dropout_nested_resid_models.R
+++ r_scripts/dropout_nested_resid_models.R
@@ -27,3 +27,26 @@ model2 <- lm(residuals ~ assessment_passing_fraction + global_item_time_commitme
 
 # Summarize the results
 summary(model2)
+
+# revise the code----
+a$dropout_percentage_log = log(a$dropout_percentage + 0.0001)
+model1 <- lmer(dropout_percentage_log ~ graded_item_order + (1 + graded_item_order | course_id), data = a)
+
+# Extract residuals
+a$residuals <- resid(model1)
+
+model1a <- glmer(dropout_percentage ~ graded_item_order + (1 | course_id), data = a, family = poisson(link = "log"))
+
+a$residuals2 <- resid(model1a)
+# Fit the second model with assessment-related features
+model2 <- lmer(residuals ~ assessment_passing_fraction + global_item_time_commitment + 
+               question_counts + checkbox_percentage + codeExpression_percentage + 
+               math.expression_percentage + mcq_percentage + mcqReflect_percentage + 
+               reflect_percentage + regex_percentage + single.numeric_percentage + 
+               text.exact.match_percentage + (1 | course_id), data = a)
+
+# Summarize the results
+summary(model2)
+
+# extract fitted value from model1
+a$predictions <- exp(predict(model1))
